4930c9a4cd1912ff2a25cfcfb2caef4ba0cfc526
gilberto veras <gilberto.veras@aol.com>
2019-10-07 18:57:50 -0400

    There's a problem, I don't know what :(

--- src/main.lua
+++ src/main.lua
@@ -69,7 +69,7 @@ function shortest_distance(eye, direction, rstart, rend)
 end
 
 function sdf_scene(ray)
-    return sphere_sdf(ray, 100)
+    return sphere_sdf(ray, Vector:new(100, 100, 100))
 end
 
 function sphere_sdf(point, radius)
@@ -78,5 +78,31 @@ end
 
 function box_sdf(point, box)
     local dist = point:abs() - box
-    return (dist:max(0)).magnitude() + math.min(math.max(dist.x, math.max(dist.y, dist.z)), 0)
+    return dist:max(0)):magnitude() + math.min(math.max(dist.x, math.max(dist.y, dist.z)), 0) 
+end
+
+function rounded_box_sdf(point, box, radius)
+    local dist = point:abs() - box
+    return dist:max(0):magnitude() - r + math.min(math.max(dist.x, math.max(dist.y, dist.z)), 0)
+end
+
+function torus(point, torus)
+    local pxz = Vector:new(point.x, point.z, 0);
+    local q = Vector:new(norm(pxz) - torus.x, point.y, 0);
+    return norm(q) - torus.y;
+end
+
+--sc is a vec2 leave z 0
+function capped_torus(point, torus, radiusa, radiusb)
+    local pxy = Vector:new(point.x, point.y, 0)
+    local px = math.abs(point.x)
+    local k = 0
+        
+    if torus.y*point.x > torus.x*point.y then
+        k = pxy:dot(torus)
+    else
+        k = (pxy):magnitude()
+    end
+
+    return math.sqrt(point:dot(point) + radiusa*radiusa - 2.0*radiusa*k) - radiusb
 end
